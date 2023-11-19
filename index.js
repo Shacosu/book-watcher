8070143ccdfb99b925faf3ed5db49ac87ae916d3
Shacosu <sh4c0p@gmail.com>
2023-11-19 20:27:27 -0300

    Remove node-cron dependency and update main function call

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import cron from 'node-cron';
 import getBooks from "./helpers/getBooks.js";
 import updateBooks from "./helpers/updateBooks.js";
 
@@ -14,9 +13,4 @@ async function main() {
     console.log(`Se ha finalizado el proceso de actualización con ${booksUpdated} libros actualizados ${new Date().toLocaleString("es-CL", { timeZone: "America/Santiago"})}`);
 }
 
-// Ejecutar la función `main` cada 10 minutos con la zona horaria de America/Santiago
-cron.schedule('*/10 * * * *', () => {
-    main();
-}, {
-    timezone: "America/Santiago"
-});
+main();
